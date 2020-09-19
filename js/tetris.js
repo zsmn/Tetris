66edfa339155c65b55623419d78f7dd27fbadf80
Víctor Hugo <vhms@cin.ufpe.br>
2020-09-19 16:04:19 -0300

    Drawing Tetrominoes

--- js/tetris.js
+++ js/tetris.js
@@ -3,6 +3,60 @@ const width = 10
 const height = 20
 
 // Tetrominoes
+const Ipiece = [
+  [width, width+1, width+2, width+3],
+  [2, width+2, 2*width+2, 3*width+2],
+  [2*width, 2*width+1, 2*width+2, 2*width+3],
+  [1, width+1, 2*width+1, 3*width+1]
+]
+
+const Jpiece = [
+  [0, width, width+1, width+2],
+  [2, 1, width+1, 2*width+1],
+  [width, width+1, width+2, 2*width+2],
+  [1, width+1, 2*width+1, 2*width]
+]
+
+const Lpiece = [
+  [width, width+1, width+2, 2],
+  [1, width+1, 2*width+1, 2*width+2],
+  [2*width, width, width+1, width+2],
+  [0, 1, width+1, 2*width+1]
+]
+
+const Opiece = [
+  [1,2,width+1,width+2],
+  [1,2,width+1,width+2],
+  [1,2,width+1,width+2],
+  [1,2,width+1,width+2]
+]
+
+const Spiece = [
+  [2, 1, width+1, width],
+  [1, width+1, width+2, 2*width+2],
+  [2*width, 2*width+1, width+1, width+2],
+  [0, width, width+1, 2*width+1]
+]
+
+const Tpiece = [
+  [1, width, width+1, width+2],
+  [1, width+1, 2*width+1, width+2],
+  [width, width+1, width+2, 2*width+1],
+  [1, width+1, 2*width+1, width]
+]
+
+const Zpiece = [
+  [0, 1, width+1, width+2],
+  [2, width+2, width+1, 2*width+1],
+  [width, width+1, 2*width+1, 2*width+2],
+  [1, width+1, width, 2*width]
+]
+
+// Collors pallete
+const quad = ['qpurple', 'qgreen', 'qred', 'qorange', 'qyellow', 'qblue', 'qbrown']
+
+// Tetrominoes vector
+const theTetrominoes = [Ipiece, Jpiece, Lpiece, Opiece, Spiece, Tpiece, Zpiece]
 
 document.addEventListener('DOMContentLoaded', () => {
 	// Creating grid
@@ -19,4 +73,3 @@ document.addEventListener('DOMContentLoaded', () => {
   const startButton  = document.querySelector('#start-button')
   
 })
-
